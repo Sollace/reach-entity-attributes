ea031516b1ccb4c6c668bb5aa6e7b961e2705a50
Sollace <sollacea@gmail.com>
2023-06-03 12:13:41 +0100

    Update to 1.20-pre6

--- src/main/java/com/jamieswhiteshirt/reachentityattributes/mixin/InventoryValidationMixin.java
+++ src/main/java/com/jamieswhiteshirt/reachentityattributes/mixin/InventoryValidationMixin.java
@@ -1,30 +1,20 @@
 package com.jamieswhiteshirt.reachentityattributes.mixin;
 
 import com.jamieswhiteshirt.reachentityattributes.ReachEntityAttributes;
-import net.minecraft.block.entity.AbstractFurnaceBlockEntity;
-import net.minecraft.block.entity.BrewingStandBlockEntity;
 
-import net.minecraft.block.entity.LootableContainerBlockEntity;
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.Inventory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
-@Mixin(value = {
-    AbstractFurnaceBlockEntity.class,
-    BrewingStandBlockEntity.class,
-    LootableContainerBlockEntity.class,
-    PlayerInventory.class
-}, targets = {
-    "net.minecraft.block.entity.LecternBlockEntity$1"
-})
-abstract class InventoryValidationMixin implements Inventory {
+@Mixin(Inventory.class)
+interface InventoryValidationMixin {
     @ModifyConstant(
-        method = "canPlayerUse(Lnet/minecraft/entity/player/PlayerEntity;)Z",
-        require = 1, allow = 1, constant = @Constant(doubleValue = 64.0))
-    private static double getActualReachDistance(final double reachDistance, final PlayerEntity player) {
-        return ReachEntityAttributes.getSquaredReachDistance(player, reachDistance);
+        method = "canPlayerUse(Lnet/minecraft/block/entity/BlockEntity;Lnet/minecraft/entity/player/PlayerEntity;)Z",
+        require = 1, allow = 1, constant = @Constant(intValue = 8))
+    private static int getActualReachDistance(final int reachDistance, final BlockEntity blockEntity, final PlayerEntity player) {
+        return (int)ReachEntityAttributes.getReachDistance(player, reachDistance);
     }
 }
--- src/main/java/com/jamieswhiteshirt/reachentityattributes/mixin/PlayerInventoryValidationMixin.java
+++ src/main/java/com/jamieswhiteshirt/reachentityattributes/mixin/PlayerInventoryValidationMixin.java
@@ -1,22 +1,15 @@
 package com.jamieswhiteshirt.reachentityattributes.mixin;
 
 import com.jamieswhiteshirt.reachentityattributes.ReachEntityAttributes;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.block.entity.BlockEntityType;
-import net.minecraft.block.entity.EnderChestBlockEntity;
+
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.util.math.BlockPos;
+import net.minecraft.entity.player.PlayerInventory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
-@Mixin(EnderChestBlockEntity.class)
-abstract class EnderChestBlockEntityMixin extends BlockEntity {
-    EnderChestBlockEntityMixin(final BlockEntityType<?> type, final BlockPos pos, final BlockState state) {
-        super(type, pos, state);
-    }
-
+@Mixin(PlayerInventory.class)
+abstract class PlayerInventoryValidationMixin {
     @ModifyConstant(
         method = "canPlayerUse(Lnet/minecraft/entity/player/PlayerEntity;)Z",
         require = 1, allow = 1, constant = @Constant(doubleValue = 64.0))
